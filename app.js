cf7c7380cba98dfd65bd9090ac67204774110132
PedroDSimoes <113497762+PedroDSimoes@users.noreply.github.com>
2023-04-03 07:49:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,334 @@
+const EasyQuestions = [  
+  {    
+    question: "What color is an emerald?",    
+    choices: ["Green", "Red", "Blue", "Yellow"],
+    answer: "Green"
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    choices: ["Mars", "Jupiter", "Saturn", "Venus"],
+    answer: "Mars"
+  },
+  {
+    question: "What is the capital city of Spain?",
+    choices: ["Barcelona", "Madrid", "Seville", "Valencia"],
+    answer: "Madrid"
+  },
+  {
+    question: "Who is Mickey Mouse's girlfriend?",
+    choices: ["Minnie Mouse", "Daisy Duck", "Clarabelle Cow", "April Duck"],
+    answer: "Minnie Mouse"
+  },
+  {
+    question: "Who wrote the Harry Potter series of books?",
+    choices: ["Stephen King", "J.K. Rowling", "George R.R. Martin", "Dan Brown"],
+    answer: "J.K. Rowling"
+  },
+  {
+    question: "What type of animal is a Komodo dragon?",
+    choices: ["Lizard", "Snake", "Crocodile", "Turtle"],
+    answer: "Lizard"
+  },
+  {
+    question: "In which sport is a shuttlecock used?",
+    choices: ["Badminton", "Tennis", "Table Tennis", "Volleyball"],
+    answer: "Badminton"
+  },
+  {
+    question: "Who was the first person to step on the moon?",
+    choices: ["Buzz Aldrin", "Neil Armstrong", "Michael Collins", "Alan Shepard"],
+    answer: "Neil Armstrong"
+  },
+  {
+    question: "What type of animal is a bald eagle?",
+    choices: ["Eagle", "Hawk", "Falcon", "Owl"],
+    answer: "Eagle"
+  },
+  {
+    question: "What is the smallest country in the world?",
+    choices: ["Monaco", "Nauru", "San Marino", "Vatican City"],
+    answer: "Vatican City"
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    choices: ["Pablo Picasso", "Vincent van Gogh", "Leonardo da Vinci", "Claude Monet"],
+    answer: "Leonardo da Vinci"
+  },
+  {
+    question: "What is the name of the famous clock tower in London?",
+    choices: ["Big Ben", "Westminster Tower", "Clock Tower", "London Tower"],
+    answer: "Big Ben"
+  },
+  {
+    question: "What is the largest ocean in the world?",
+    choices: ["Indian Ocean", "Arctic Ocean", "Pacific Ocean", "Atlantic Ocean"],
+    answer: "Pacific Ocean"
+  },
+  {
+    question: "What is the name of the fictional city where Batman is based?",
+    choices: ["Gotham City", "Metropolis", "Star City", "Central City"],
+    answer: "Gotham City"
+  },
+  {
+    question: "What is the capital city of Australia?",
+    choices: ["Melbourne", "Brisbane", "Sydney", "Canberra"],
+    answer: "Canberra"
+  }
+];
+
+const MediumQuestions = [  
+  {    
+    question: "What is the smallest planet in our solar system?",    
+    choices: ["Mercury", "Mars", "Venus", "Earth"],
+    answer: "Mercury"
+  },
+  {
+    question: "What is the largest organ in the human body?",
+    choices: ["Liver", "Brain", "Lungs", "Skin"],
+    answer: "Skin"
+  },
+  {
+    question: "What is the currency of Japan?",
+    choices: ["Yuan", "Won", "Yen", "Dollar"],
+    answer: "Yen"
+  },
+  {
+    question: "Who is the creator of the TV series 'The Simpsons'?",
+    choices: ["Matt Groening", "Seth MacFarlane", "Trey Parker", "Mike Judge"],
+    answer: "Matt Groening"
+  },
+  {
+    question: "What is the name of the world's largest desert?",
+    choices: ["Gobi Desert", "Sahara Desert", "Arabian Desert", "Kalahari Desert"],
+    answer: "Sahara Desert"
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    choices: ["Ag", "Au", "Cu", "Pt"],
+    answer: "Au"
+  },
+  {
+    question: "What is the name of the largest volcano on Earth?",
+    choices: ["Mauna Kea", "Mount Kilimanjaro", "Mount Everest", "Mauna Loa"],
+    answer: "Mauna Loa"
+  },
+  {
+    question: "What is the name of the first artificial satellite to be launched into space?",
+    choices: ["Explorer 1", "Vanguard 1", "Sputnik 1", "Telstar 1"],
+    answer: "Sputnik 1"
+  },
+  {
+    question: "What is the name of the mountain range that runs through South America?",
+    choices: ["Rocky Mountains", "Andes Mountains", "Himalayas", "Alps"],
+    answer: "Andes Mountains"
+  },
+  {
+    question: "What is the name of the capital city of New Zealand?",
+    choices: ["Wellington", "Auckland", "Christchurch", "Queenstown"],
+    answer: "Wellington"
+  },
+  {
+    question: "What is the name of the longest river in Asia?",
+    choices: ["Mekong River", "Yangtze River", "Ganges River", "Indus River"],
+    answer: "Yangtze River"
+  },
+  {
+    question: "What is the name of the scientist who developed the theory of relativity?",
+    choices: ["Isaac Newton", "Albert Einstein", "Galileo Galilei", "Nikola Tesla"],
+    answer: "Albert Einstein"
+  },
+  {
+    question: "What is the name of the largest city in Canada?",
+    choices: ["Ottawa", "Toronto", "Montreal", "Vancouver"],
+    answer: "Toronto"
+  },
+  {
+    question: "What is the name of the painting by Edvard Munch that features a figure screaming on a bridge?",
+    choices: ["The Scream", "The Shout", "The Cry", "The Howl"],
+    answer: "The Scream"
+  },
+  {
+    question: "What is the name of the highest mountain in North America?",
+    choices: ["Denali", "Mount Saint Elias", "Mount Foraker", "Mount Blackburn"],
+    answer: "Denali"
+  },
+]
+
+const HardQuestions = [
+{
+  question: "What is the capital of Chad?",
+  choices: ["N'Djamena", "Bangui", "Kigali", "Brazzaville"],
+  answer: "N'Djamena"
+},
+{
+  question: "Which English monarch was known as 'The Virgin Queen'?",
+  choices: ["Queen Mary I", "Queen Elizabeth I", "Queen Anne", "Queen Victoria"],
+  answer: "Queen Elizabeth I"
+},
+{
+  question: "What is the highest point in Antarctica?",
+  choices: ["Vinson Massif", "Mount Erebus", "Mount Sidley", "Mount Kirkpatrick"],
+  answer: "Vinson Massif"
+},
+{
+  question: "Who invented the rabies vaccine?",
+  choices: ["Louis Pasteur", "Robert Koch", "Alexander Fleming", "Edward Jenner"],
+  answer: "Louis Pasteur"
+},
+{
+  question: "What is the smallest bone in the human body?",
+  choices: ["Stapes", "Coccyx", "Femur", "Ulna"],
+  answer: "Stapes"
+},
+{
+  question: "What is the name of the largest canyon in the solar system?",
+  choices: ["Valles Marineris", "Grand Canyon", "Canyonlands", "Barringer Crater"],
+  answer: "Valles Marineris"
+},
+{
+  question: "What is the name of the first successful artificial satellite launched into orbit?",
+  choices: ["Sputnik 1", "Explorer 1", "Vanguard 1", "Vostok 1"],
+  answer: "Sputnik 1"
+},
+{
+  question: "What is the name of the first woman to climb Mount Everest?",
+  choices: ["Junko Tabei", "Reinhold Messner", "Edmund Hillary", "Tenzing Norgay"],
+  answer: "Junko Tabei"
+},
+{
+  question: "What is the name of the first spacecraft to visit Pluto?",
+  choices: ["New Horizons", "Voyager 1", "Cassini-Huygens", "Galileo"],
+  answer: "New Horizons"
+},
+{
+  question: "What is the name of the world's largest living organism?",
+  choices: ["Blue Whale", "Giant Sequoia", "Great Barrier Reef", "Honey Fungus"],
+  answer: "Honey Fungus"
+},
+{
+  question: "What is the name of the first person to discover a planet using a telescope?",
+  choices: ["Johannes Kepler", "Galileo Galilei", "William Herschel", "Isaac Newton"],
+  answer: "William Herschel"
+},
+{
+  question: "What is the name of the first element on the periodic table?",
+  choices: ["Hydrogen", "Lithium", "Oxygen", "Carbon"],
+  answer: "Hydrogen"
+},
+{
+  question: "What is the name of the largest impact crater on Earth?",
+  choices: ["Chicxulub Crater", "Vredefort Crater", "Sudbury Basin", "Popigai Crater"],
+  answer: "Vredefort Crater"
+},
+{
+  question: "What is the name of the first person to sail solo around the world?",
+  choices: ["Francis Chichester", "Robin Knox-Johnston", "Bernard Moitessier", "Joshua Slocum"],
+  answer: "Joshua Slocum"
+},
+{
+  question: "Who is the author of the book 'The Republic'?",
+  choices: ["Aristotle", "Socrates", "Plato", "Epicurus"],
+  answer: "Plato"
+}
+]
+
+  document.getElementById("easy-btn").addEventListener("click", function() {
+    startQuiz(EasyQuestions);
+  });
+  
+  document.getElementById("medium-btn").addEventListener("click", function() {
+    startQuiz(MediumQuestions);
+  });
+
+  document.getElementById("hard-btn").addEventListener("click", function() {
+    startQuiz(HardQuestions);
+  });
+
+  function startQuiz(questions) {
+    let currentQuestion = 0;
+    let score = 0;
+    let timerSeconds = 10;
+  
+    const questionElement = document.getElementById("question");
+    const choicesElement = document.getElementById("choices");
+    const scoreElement = document.getElementById("score");
+    const timerElement = document.getElementById("timer");
+  
+    let timerInterval;
+
+    function showQuestion() {
+      clearInterval(timerInterval);
+      const question = questions[currentQuestion];
+      questionElement.innerText = question.question;
+      choicesElement.innerHTML = "";
+      const choicesContainer = document.createElement("div");
+      choicesContainer.className = "choices-container";
+      for (let i = 0; i < question.choices.length; i++) {
+        const choice = question.choices[i];
+        const button = document.createElement("button");
+        button.innerText = choice;
+        button.addEventListener("click", function() {
+          checkAnswer(choice);
+        });
+        choicesContainer.appendChild(button);
+      }
+      choicesElement.appendChild(choicesContainer);
+      timerSeconds = 10;
+      timerElement.innerText = `Time remaining: ${timerSeconds} seconds`;
+      timerInterval = setInterval(() => {
+        timerSeconds--;
+        if (timerSeconds > 0) {
+          timerElement.innerText = `Time remaining: ${timerSeconds} seconds`;
+        } else {
+          clearInterval(timerInterval);
+          checkAnswer(null);
+        }
+      }, 1000);
+      document.getElementById("easy-btn").style.display = "none";
+      document.getElementById("medium-btn").style.display = "none";
+      document.getElementById("hard-btn").style.display = "none";
+    }
+  
+    function checkAnswer(choice) {
+      const question = questions[currentQuestion];
+      if (choice === question.answer) {
+        score++;
+      }
+      currentQuestion++;
+      if (currentQuestion < questions.length) {
+        showQuestion();
+      } else {
+        endQuiz();
+      }
+    }
+  
+    function endQuiz() {
+      questionElement.innerText = "Quiz complete!";
+      choicesElement.innerHTML = "";
+      scoreElement.innerText = `You scored ${score} out of ${questions.length}!`;
+      timerElement.style.display = "none";
+      document.getElementById("play-again").style.display = "block";
+    }
+  
+    function resetQuiz() {
+      currentQuestion = 0;
+      score = 0;
+      timerSeconds = 10;
+      clearInterval(timerInterval);
+      questionElement.innerText = "";
+      choicesElement.innerHTML = "";
+      scoreElement.innerText = "";
+      timerElement.innerHTML = "";
+      timerElement.style.display = "block";
+      document.getElementById("play-again").style.display = "none";
+      document.getElementById("easy-btn").style.display = "block";
+      document.getElementById("medium-btn").style.display = "block";
+      document.getElementById("hard-btn").style.display = "block";
+    }
+  
+    showQuestion();
+  
+    document.getElementById("play-again").addEventListener("click", function() {
+      resetQuiz();
+    });
+  }    
